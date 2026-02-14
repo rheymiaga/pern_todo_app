e0d8a9a1ada1572b10cb1bc97d791506cc378283
Rhey Louie Miaga <mrheylouie@gmail.com>
2026-02-14 14:46:49 +0800

    updated server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,9 +10,7 @@ const PORT: number = Number(process.env.PORT) || 5000;
 const app: Application = express();
 
 // Middleware
-app.use(cors({
-    origin: process.env.FRONTEND_URL
-}));
+app.use(cors());
 app.use(express.json());
 
 // Health check route
